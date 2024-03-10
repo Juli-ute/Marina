f965b06afbd94315c1f20af3ccede61f182ca310
Juli-ute <161311679+Juli-ute@users.noreply.github.com>
2024-03-10 23:36:22 +0300

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "easy.h"
+using namespace std;
+
+//1
+void itc_name(){
+cout << "Marina";
+}
+
+//2
+void itc_fio(){
+itc_name();
+cout << " Roslova Necolaevna";
+
+}
+
+//3
+int itc_abs(int ch){
+int risult;
+if(ch >= 0){
+    return ch;
+}
+else{
+    risult = ch * -1;
+    return risult;
+}
+
+}
+
+//4
+double itc_fabs(double ch){
+double risult;
+if(ch >= 0){
+    return ch;
+}
+else{
+    risult = ch * -1;
+    return risult;
+}
+
+}
+
+//5
+int itc_revnbr(int ch){
+int a, b, c, risult;
+a = ch%10;
+ch = ch / 10;
+b = ch % 10;
+ch = ch / 10;
+c = ch % 10;
+risult = a * 100 + b * 10 + c;
+return risult;
+}
--- easy.h
+++ easy.h
@@ -1,6 +1,19 @@
 #ifndef EASY_H_INCLUDED
 #define EASY_H_INCLUDED
 
-void its_name();
+void itc_name();
+void itc_fio();
+int itc_abs(int ch);
+double itc_fabs(double ch);
+int itc_revnbr(int ch);
+bool itc_iseven(int ch);
+int itc_max(int ch, int ch1);
+int itc_min(int ch, int ch1);
+double itc_fmax(double ch, double ch1);
+double itc_fmin(double ch, double ch1);
+int itc_sqrt(int ch);
+int itc_skv(int ch);
+int itc_spr(int a, int b);
+int itc_str(int a, int b, int c);
 
 #endif // EASY_H_INCLUDED
